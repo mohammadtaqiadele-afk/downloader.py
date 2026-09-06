17a684565197c87b3e0b042e9c6748cd3cd9725e
mohammadtaqiadele-afk <mohammadtaqiadele@gmail.com>
2026-09-06 13:06:56 +0330

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -61,7 +61,7 @@ async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 'outtmpl': 'downloaded_media.%(ext)s',
                 'noplaylist': True,
                 'quiet': True,
-                'extractor_args': {'youtube': {'player_client': ['android', 'web']}},
+                'extractor_args': {'youtube': {'player_client': ['mweb']}},
             }
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(url, download=True)
